f5f9db1281792182629ee989d8e3ce67085f190b
sudheer <nagacs327@gmail.com>
2025-04-30 20:31:39 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,92 @@
+// +,
+// -,
+// /,
+// %,*
+
+x = 6
+y = 8
+Z =x+y //Executed But it will not display 
+//console.log(30+20+20+"66") All these things added and come 70 and70 is coverted into string cocatnate is hapenning 7066
+// console.log(44+45+"66"+55) //896655 First two added then it became string
+// x = 7
+// y = "9"
+// z = x-y
+// console.log(z) //Here stringbcoverts to number
+
+// z = 5 - "Sud"
+// console.log(z) //NAN Not a number 
+// z = 5 * "5"
+// console.log(z)  //25 
+// z = 25% 10
+// console.log(z) //Multiply
+
+
+//Increment operator
+// x = 6
+// x++ //x = x+1
+// console.log(x) //we get 7 Its incremental operator ++ It will increase operand value by 1
+// //Decrement operator
+// x = 6
+// x-- //x = x-1
+// console.log(x) //we get 5 Its Decrement operator -- It will decrease operand value by 1
+// Comparision operators will return true or false
+//== or === will be used fr comparision
+//== it will compare only values it doesnot calculate datatype console.log(6 == "6")true
+//===it will compare only values it calculate value and datatype console.log(6 === "6") false
+//console.log(4!=6) != not equal
+//** is used for power
+// ====================================================================================================================
+//Ternary operators or Conditional
+stu1 = "sudheer"
+stu2 = "kiran"
+Finalstu = 2>5 ? stu1:stu2 //Here the conditon is fail so instead of manu it will show ravi
+console.log(Finalstu)
+stu1 = "sudheer"
+stu2 = "kiran"
+Finalstu = 5>3 ? stu1:stu2 //Here the conditon is pass so Manu  will show
+console.log(Finalstu)
+stu1 = "sudheer"
+stu2 = "kiran"
+Finalstu = 10 == "10"? stu1:stu2 //Here the conditon is pass so Manuwill show
+console.log(Finalstu)
+// If the condition is pass or true value 1 will assign If condition is fail or false value 2 is assigned
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
