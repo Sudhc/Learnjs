840cf232089f5dc56ca6fe3fd86bad2b640e7cda
sudheer <nagacs327@gmail.com>
2025-04-30 22:17:47 +0530

    variables

---
+++ varables.js
@@ -0,0 +1,82 @@
+// var:
+
+// {
+//     var x = 'sudheer'
+
+// if(9>8){
+//     console.log(x)
+// }
+// }
+
+// {
+  
+
+// if(9>8){
+//       var x = 'sudheer'
+//     console.log(x)
+// }
+// console.log(x)
+// }
+
+// let:
+    //    error
+// {
+// if(9>8){
+//       let x = 'sudheer'
+//     console.log(x)
+// }
+// console.log(x)
+// }
+// {
+       //error
+//     if(9>8){
+//           const x = 'sudheer'
+//         console.log(x)
+//     }
+//     console.log(x)
+//     }
+
+// Var -  Variable has global scope
+// let and const -Blocked scope
+
+// var stuname = "deepak"
+// stuname = "sudheer"
+// console.log(stuname)
+// // var - Here Re-assign the different value is possible
+
+// if(true){
+//     let x = "john"
+//  x = "Ravi"
+//  console.log(x)
+
+// }
+//  Re-Asign with different value is possible with let and var 
+
+// if(true){
+//     const x = "john"
+//  x = "Ravi"
+//  console.log(x)
+
+// }
+//  Re-Asign with different value is not possible with const editor will not throw eeror but compiler will do
+
+// var orderno = "75648"
+// var orderno = "67576"
+// console.log(orderno)
+// Declaration of same var is possible with var
+
+// let orderno = "75648"
+// let orderno = "67576"
+// console.log(orderno)
+// Declaration of same var is not possible with let
+
+// const orderno = "75648"
+// const orderno = "67576"
+// console.log(orderno)
+// Declaration of same var is not possible with const
+
+ 
+
+
+
+
