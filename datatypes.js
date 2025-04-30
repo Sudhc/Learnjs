4ee2c8d016d9b41a326090813128a14d5232e94a
sudheer <nagacs327@gmail.com>
2025-04-30 16:59:53 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,35 @@
+// x = 10
+// y=20
+
+// // typeof
+// // primitive data types
+// // String number undefined null boolen 
+// // datatype of null is object
+// console.log(x)               //Number
+// console.log(typeof(x))        
+//  x = "23"
+//  console.log(typeof(x))     //string
+//  x = true
+//  console.log(typeof(x))          //boolean
+//  x
+//  console.log(typeof(x))         //undefined
+//  var x ;
+//  console.log(typeof(x))           //undefined
+// var x = undefined;
+//  console.log(typeof(x))             //undefined          
+// var z = null
+// console.log(typeof(z))              //data type of null is object it is bug in javascript js is compared with c language
+//Difference b/w undefined and null
+//null = empty
+//Undefined = not defined yet but not empty
+// Non - primitive datatypes
+// 1. Arrays
+// 2. Objects
+// 3.Regular Expressions
+// arr =[] //empty array
+arr1 = ["raju", 21 ,"bglr" ,true] //these are hetiro elements .... Beacuse multiple data types
+// arr2 = [1,2,3,35,3,36,7] //Homogenious elements beacuase all  r numbers
+// arr3 = ["Raju", "suresh","naresh"] //homogeous elements all are strings
+console.log(typeof(arr1[0,1]))     //it will give it is number
+// console.log(arr1[1])  // in this way we get specific name
+//Moduler -% - Will get
