df953433ae7396d5d6d21449f42cf4327602ad4e
sudheer <nagacs327@gmail.com>
2025-05-04 04:25:44 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,59 @@
+// for
+// while
+// do=while
+// If you want  to run something for multiple times we can go for loops
+
+
+// for(initialization; condition; increment/decrement){
+
+// }
+
+// for(let i=1; i<10; i++){
+
+//     console.log(i)
+//     console.log("sudheer")
+// }
+
+// for(let i=10; i<10; i--){
+
+//     console.log(i)
+//     console.log("sudheer")
+// }
+
+
+// while (condition){
+
+// }
+
+// i =0
+// while (i<10){
+//     console.log(i)
+//     console.log('sudheer')
+//     i++
+// }
+
+// i =11
+// while (i>10){
+//     console.log(i)
+//     console.log('sudheer')
+//   i--
+// }
+
+// h
+
+
+    // For loop and while if the condition is fail the it wil not exicute single time also
+// do while it will exicute atleast one time after that
+// Does not matter pass or fail atleast one time it will execute.
+
+
+// Apart from that we have 'for in', 'for of' and 'for each'
+
+
+
+
+
+
+
+
+
